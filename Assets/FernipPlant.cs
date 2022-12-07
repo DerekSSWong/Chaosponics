4dce19534b8715f8521234bed5163adc1bf48e59
Derek Wong <ssderekwong@gmail.com>
2022-12-07 15:10:47 +0000

    Meta update

---
+++ Assets/FernipPlant.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FernipPlant : Plant
+{
+	public override Invoice generatePrimeIntake(Nutrient soilN)
+	{
+		Invoice intake = PrimeInvoice;
+		intake.mult(soilN.getSaltWeight());
+		return intake;
+	}
+}
