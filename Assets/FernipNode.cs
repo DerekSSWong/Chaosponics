a68428539269ba495f4cb42b2981287eba4ff94f
Derek Wong <ssderekwong@gmail.com>
2022-12-11 16:40:52 +0000

    Node update

---
+++ Assets/FernipNode.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FernipNode : FruitNode
+{
+	public override void crystallise()
+	{
+		//Determine yield based on agent
+		//Modify purity and yield based on salt/brimstone
+		float concentration = getAgentConcentration();
+		float purityBuff = sigmoidConcentration(concentration);
+		Purity += BasePurity*purityBuff;
+		Purity = Mathf.Max(Purity, MaxPurity);
+	}
+		
+}
